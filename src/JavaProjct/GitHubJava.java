16436f1a6c5f53ad4a803d2a3a68d05f15e7cdaa
omarkdj <omarkdj@gmail.com>
2020-02-02 01:40:59 -0500

    my first setap

---
+++ src/JavaProjct/GitHubJava.java
@@ -0,0 +1,24 @@
+package JavaProjct;
+
+public class GitHubJava {
+
+	public static void main(String[] args) {
+	
+		
+		int couneter = 0;
+		
+		while ( couneter < 10 ) {
+			
+			System.out.println( couneter );
+			couneter++;
+		}
+		
+		
+	//	whil loop	
+		
+		
+		
+
+	}
+
+}
